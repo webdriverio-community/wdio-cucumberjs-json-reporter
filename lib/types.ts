ee0d929d75fe47b3e729bca25106a53569940a2d
Bhaslamb <baskercarrer@gmail.com>
2022-01-31 20:14:41 +0100

    * added optional property to disable hooks information on JSON generation

--- lib/types.ts
+++ lib/types.ts
@@ -3,4 +3,5 @@ import { Reporters } from '@wdio/types';
 export interface CucumberJsJsonReporterInterface extends Reporters.Options {
     jsonFolder?: string;
     language?: string;
+    disableHooks?: boolean;
 }
