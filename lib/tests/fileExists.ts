56b069c442f5a16c485c8be8b946e76f5721e6d6
bjelugbo <88048710+bjelugbo@users.noreply.github.com>
2023-04-06 09:11:08 -0400

    Update lib/tests/fileExists.ts
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- lib/tests/fileExists.ts
+++ lib/tests/fileExists.ts
@@ -4,7 +4,6 @@ import { accessSync } from 'fs';
  * Check if a file exists
  */
 export const fileExists = ( filePath: string ): boolean => {
-   // const fse = require('fs-extra');
     try {
         accessSync( filePath );
         return true;
