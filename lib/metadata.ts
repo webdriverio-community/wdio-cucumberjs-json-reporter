64e5f9f8e0da182b69b2103425a89ed8166c37b7
bjelugbo <88048710+bjelugbo@users.noreply.github.com>
2023-04-06 12:36:40 -0400

    Update lib/metadata.ts
    Co-authored-by: Christian Bromann <git@bromann.dev>

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -22,12 +22,6 @@ export class Metadata {
             = Object.prototype.hasOwnProperty.call(data.config.capabilities, 'cjson:metadata') ? // Fixes: https://github.com/webdriverio-community/wdio-cucumberjs-json-reporter/issues/73 Fallback
             (data.config.capabilities as cjson_metadata)
             : (browser.options as WebdriverIOExtended)?.requestedCapabilities?.cjson_metadata;
-
-      /*   const w3cCaps: cjson_metadata | undefined =
-            !Object.prototype.hasOwnProperty.call( data.config.capabilities, 'cjson:metadata' ) ?
-                ( browser.options as WebdriverIOExtended )?.requestedCapabilities?.cjson_metadata :
-                data.config.capabilities['cjson:metadata'] as cjson_metadata; */
-
         const metadata: cjson_metadata =
             (currentConfigCapabilities as W3CCapabilitiesExtended)?.cjson_metadata ||
             w3cCaps || // When an app is used to test
