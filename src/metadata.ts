6dbb5f9092ddfe9697b4b77ecf00ee2ef32aac7c
Jelugbo B <jelugbo@gmail.com>
2023-05-10 23:59:44 -0400

    reformatting code

--- src/metadata.ts
+++ src/metadata.ts
@@ -18,7 +18,8 @@ export class Metadata {
             // Fixes: https://github.com/webdriverio-community/wdio-cucumberjs-json-reporter/issues/73
             ? data.config.capabilities['cjson:metadata'] as cjson_metadata
             // Fallback
-            : ( browser.options as WebdriverIOExtended )?.requestedCapabilities?.cjson_metadata : {}
+            : ( browser.options as WebdriverIOExtended )?.requestedCapabilities?.cjson_metadata
+            : {}
         const metadata: cjson_metadata = ( currentConfigCapabilities as W3CCapabilitiesExtended )?.cjson_metadata
             || w3cCaps // When an app is used to test
             || ( optsCaps as DesiredCapabilitiesExtended )?.cjson_metadata // devtools
