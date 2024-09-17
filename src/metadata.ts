974d61b50d02b2fe53766976d463990bf74ff02c
Hanna Tarasevich <htarasevich@deloitte.com>
2024-09-17 20:48:15 +0200

    Fix 582 issue

--- src/metadata.ts
+++ src/metadata.ts
@@ -18,7 +18,7 @@ export class Metadata {
                 // Fixes: https://github.com/webdriverio-community/wdio-cucumberjs-json-reporter/issues/73
                 ? data.capabilities['cjson:metadata'] as cjson_metadata
                 // Fallback
-                : (data.instanceOptions as WebdriverIOExtended)?.requestedCapabilities?.cjson_metadata
+                : (data.instanceOptions as unknown as WebdriverIOExtended)?.requestedCapabilities?.cjson_metadata
             : {}
         const metadata: cjson_metadata = (currentConfigCapabilities as W3CCapabilitiesExtended)?.cjson_metadata
             || w3cCaps // When an app is used to test
